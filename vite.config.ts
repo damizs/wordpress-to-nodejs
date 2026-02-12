48b917b8124137f5e1b53f1ec7facb8c980c7f93
damizs <damizs@github.com>
2026-02-12 14:09:46 +0000

    fix: disable SSR in vite config to fix missing client manifest

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,10 @@ import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
 export default defineConfig({
   plugins: [
-    inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }),
+    inertia({ ssr: { enabled: false } }),
     react(),
     adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
   ],
-
-  /**
-   * Define aliases for importing modules from
-   * your frontend code
-   */
   resolve: {
     alias: {
       '~/': `${getDirname(import.meta.url)}/inertia/`,
